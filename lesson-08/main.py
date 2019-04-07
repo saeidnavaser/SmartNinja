000c49aea7e334f3f0cda9216b0615578c120483
saeid navaser <saeidnavaser@gmx.at>
2019-04-07 23:19:14 +0200

    new up-lessons in smart ninja kurse

---
+++ lesson-08/main.py
@@ -0,0 +1,66 @@
+#x = int(input("Gib eine Zahle ein:"))
+#y = int(input("Gib eine weiter Zahl ein: "))
+
+
+#text = "Hello world"
+#print(x+y)
+
+#print(text)
+
+#if x == y:
+   # print("text")
+#elif x > y:
+ #   print("Groeßer")
+#else:
+  #  print("bla")
+
+#if x != y:
+   # print("ungleich")
+
+
+
+
+#x = input("It is great to see you happy!")
+
+#if x == "happy":
+ #   print("Take a deep breath 3 times.")
+
+#if x == "nervous":
+ #   print(" hello")
+
+#elif x == "nervous":
+ #   print("wien")
+
+#elif x == "sad":
+   # print(" I´m not happy!")
+
+#elif x == "excited":
+   # print("yes")
+
+#elif x == "relaxed":
+ #   print("I´m here!")
+
+#else: "I don't know"
+
+x = 0
+while( x < 10):
+    print("hallo")
+    x+=1
+
+
+
+scret = 5
+
+winner = True
+while(winner):
+    guess = int(input("Guess a Number of Scret: "))
+
+if guess == scret:
+    print("congratz")
+    winner = False
+elif guess < scret:
+    print("That was the low")
+elif guess > scret:
+    print("That was to high")
+
+    str(8)
